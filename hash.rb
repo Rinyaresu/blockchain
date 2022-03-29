6de046bd17d9bb4f69c34f1994f3a57f24b6327c
Rinyaresu <79602114+Rinyaresu@users.noreply.github.com>
2022-03-29 19:40:44 -0300

    feat: create hash

--- hash.rb
+++ hash.rb
@@ -1,1 +1,24 @@
 require 'digest'
+
+p Digest::SHA256.hexdigest('hello world!')
+
+p Digest::SHA256.hexdigest('Kaique')
+
+p Digest::SHA256.hexdigest('Linhares')
+
+p Digest::SHA256.hexdigest(<<TXT)
+  Data Data Data Data Data Data
+  Data Data Data Data Data Data
+  Data Data Data Data Data Data
+  Data Data Data Data Data Data
+  Data Data Data Data Data Data
+TXT
+
+p Digest::SHA256.hexdigest('Kaique').length
+p Digest::SHA256.hexdigest('Linhares').length
+
+hex = Digest::SHA256.hexdigest('Kaique')
+
+p hex.to_i(16)
+
+p hex.to_i(16).to_s(2)
