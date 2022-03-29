913202a8f6fbbb49d343e2df3e0d84b205fd7952
Rinyaresu <79602114+Rinyaresu@users.noreply.github.com>
2022-03-29 20:00:58 -0300

    feat: create compute hash

--- block-with-proof-of-work.rb
+++ block-with-proof-of-work.rb
@@ -9,3 +9,14 @@ class Block
     @nonce, @hash = compute_hash_with_proof_of_work
   end
 
+  def compute_hash_with_proof_of_work(difficult = '0000')
+    nonce = 0
+    loop do
+      hash = Digest::SHA256.hexdigest("#{data}#{nonce}")
+      return nonce, hash if hash.start_with?(difficult)
+
+      nonce += 1
+    end
+  end
+end
+
