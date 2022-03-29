7df6df0834f53f1024a8b0899a02b4511efb34a9
Rinyaresu <79602114+Rinyaresu@users.noreply.github.com>
2022-03-29 20:00:33 -0300

    feat: created block with proof class

--- block-with-proof-of-work.rb
+++ block-with-proof-of-work.rb
@@ -1,2 +1,11 @@
 require 'digest'
 require 'pp'
+
+class Block
+  attr_reader :data, :hash, :nonce
+
+  def initialize(data)
+    @data = data
+    @nonce, @hash = compute_hash_with_proof_of_work
+  end
+
