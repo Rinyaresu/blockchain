66f11d36d635e553eb527fc3f61937f0b76414c5
Rinyaresu <79602114+Rinyaresu@users.noreply.github.com>
2022-03-30 18:28:06 -0300

    feat: create block class

--- blockchain.rb
+++ blockchain.rb
@@ -1,3 +1,24 @@
 require 'digest'
 require 'pp'
 
+class Block
+  attr_reader :data, :prev, :hash, :nonce
+
+  def initialize(data, prev)
+    @data = data
+    @prev = prev
+    @nonce, @hash = compute_hash_with_proof_of_work
+  end
+
+  def compute_hash_with_proof_of_work(difficulty = '0000')
+    nonce = 0
+    loop do
+      hash = Digest::SHA256.hexdigest("#{nonce}#{data}#{prev}")
+      if hash.start_with?(difficulty)
+        return [nonce, hash]
+      else
+        nonce += 1
+      end
+    end
+  end
+end
